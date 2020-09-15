d1336a580febf7295b42ee29bb3d4c731b56e41b
Maxime HENRY <maxime.3497@gmail.com>
2020-09-15 22:31:56 +0200

    aller les bleu

--- test.py
+++ test.py
@@ -1,11 +1,8 @@
 print"hello world"
 print"good night"
 ook je rajoute un truc pour la science
-<<<<<<< HEAD
 depuis le mac
-=======
 depuis internet
->>>>>>> origin/master
 print"something else"
 print "encore et encore "
 testttt
